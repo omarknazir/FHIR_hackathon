08d82436887d223ee4131bfd587202789c7b6bf8
unknown <omar.nazir.23@ucl.ac.uk>
2024-02-12 14:55:57 +0000

    second

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
 from fhir.resources.patient import Patient
 from fhir.resources.humanname import HumanName
 from fhir.resources.fhirdate import FHIRDate
-
-# Your data (this should be replaced with your actual data)
-data = {
-  "name": "John Doe",
-  "birth_date": "1980-01-01",
-}
+from fhir.resources.medication import Medication
+from fhir.resources.detectedissue import DetectedIssue
+from fhir.resources.allergyintolerance import AllergyIntolerance
+from fhir.resources.condition import Condition
+from fhir.resources.familymemberhistory import FamilyMemberHistory
 
 # Create a FHIR Patient resource
 patient = Patient()
-
-# Set the name
 name = HumanName()
-name.family = data["name"].split(" ")[1]
-name.given = [data["name"].split(" ")[0]]
+name.family = "Doe"
+name.given = ["John"]
 patient.name = [name]
+patient.birthDate = FHIRDate("1980-01-01")
 
-# Set the birth date
-patient.birthDate = FHIRDate(data["birth_date"])
+# Create a FHIR Medication resource
+medication = Medication()
+medication.status = "active"
+medication.code = {"text": "Aspirin"}
 
-# Now `patient` is a FHIR Patient resource
-print(patient.json(indent=2))
+# Create a FHIR DetectedIssue resource
+detected_issue = DetectedIssue()
+detected_issue.status = "final"
+
+# Create a FHIR AllergyIntolerance resource
+allergy_intolerance = AllergyIntolerance()
+allergy_intolerance.clinicalStatus = {"coding": [{"system": "http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical", "code": "active"}]}
+
+# Create a FHIR Condition resource
+condition = Condition()
+condition.clinicalStatus = {"coding": [{"system": "http://terminology.hl7.org/CodeSystem/condition-clinical", "code": "active"}]}
+
+# Create a FHIR FamilyMemberHistory resource
+family_member_history = FamilyMemberHistory()
+family_member_history.status = "completed"
+
+# Now you have FHIR resources for each of the requested types
